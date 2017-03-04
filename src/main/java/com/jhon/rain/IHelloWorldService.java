8f915858761be216882650ad9239503dffda57e8
TonyYuJhon <jhonyucoder@163.com>
2017-03-04 13:40:07 +0800

    Initial commit

---
+++ src/main/java/com/jhon/rain/IHelloWorldService.java
@@ -0,0 +1,18 @@
+package com.jhon.rain;
+
+/**
+ * <p></p>
+ *
+ * @author jiangyu
+ * @version v1.0
+ * @className IHelloWorldService
+ * @create 2017-03-02 23:09
+ */
+public interface IHelloWorldService
+{
+    /**
+     * @param name
+     * @return
+     */
+    public String sayHello(String name);
+}
